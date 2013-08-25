156bfe5f3dc0552fb48ce42259d5ae1fee090308
App Academy Student <student@appacademy.io>
2013-08-25 13:06:39 -0400

    Initialized guess and answer player to nil

--- hangman_new.rb
+++ hangman_new.rb
@@ -1,3 +1,16 @@
 class Hangman
 
+  attr_accessor :dictionary, :guess_player, :answer_player
+
+  def initialize
+    self.dictionary = File.read('dictionary.txt').split(' ')
+    self.guess_player = nil
+    self.answer_player = nil
+  end
+
+end
+
+if __FILE__ == $0
+  hangman = Hangman.new
+  p hangman.dictionary
 end
