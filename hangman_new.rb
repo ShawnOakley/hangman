34c02bdc2d6c82cad2750fd5da534caa46ec35ac
App Academy Student <student@appacademy.io>
2013-08-25 13:28:43 -0400

    Added UI for choosing type of game.  Added methods for establishing player types.

--- hangman_new.rb
+++ hangman_new.rb
@@ -8,6 +8,29 @@ class Hangman
     self.answer_player = nil
   end
 
+  def self.create_comp_game()
+    self.guess_player = ComputerPlayer.new
+    self.answer_player = HumanPlayer.new
+  end
+
+  def self.create_human_game()
+    self.guess_player = HumanPlayer.new
+    self.answer_player = ComputerPlayer.new
+  end
+
+  def choose_game()
+    puts "Please choose who will guess (Computer or Human):"
+    if /comp|computer/i.match(guessing_player)
+      self.create_comp_game
+    elsif /human|h|me/i.match(guessing_player)
+      self.create_human_game
+    else
+      puts "Invalid input.  Please enter either 'Human' or 'Computer'."
+      choose_game
+    end
+  end
+
+
 end
 
 if __FILE__ == $0
